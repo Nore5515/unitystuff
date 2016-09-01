5d77dbe3b4a83ec0376f7f3f916bca969ecc7194
20juanpls <shortwavepeak11@gmail.com>
2016-09-01 16:08:54 -0500

    Add files via upload
    this patch is almost gud, excpt it has bad ayy npc

---
+++ DaControls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DaControls : MonoBehaviour {
+	/*public float gottagofast;
+	private BoxCollider bexc;
+	private Transform thisTransform; 
+
+	// Use this for initialization
+	void Start () {
+		bexc = GetComponent<BoxCollider> ();
+	}
+	void Transform GetTransform(){
+		bexc.transform.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveX, 0.0f, moveY);
+		bexc.transform.Translate(movement * gottagofast);
+
+	}*/
+	private Transform thisTransform;
+	public float gottagofast;
+	
+	void Awake() {
+		thisTransform = transform;    
+	}
+	
+	public Transform GetTransform() {
+		return thisTransform;    
+	}
+	
+	void Update() {
+		
+		float keyboardX = Input.GetAxis("Horizontal");
+		float keyboardY = Input.GetAxis("Vertical");
+		
+		var newPos = thisTransform.position + new Vector3(keyboardX, 0, keyboardY);    
+		thisTransform.position = Vector3.MoveTowards(thisTransform.position, newPos, Time.deltaTime * gottagofast);
+	}
+}
---
+++ RunAway.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunAway : MonoBehaviour {
+	DaControls ayylmao;
+
+	Transform thisTransform;
+	public float minDistance = 10;
+	public float fest;
+	
+	void Awake() {
+		thisTransform = transform;
+		//thisTransform.position = new Vector3(2, 0, 2);
+	}
+	
+	void Start() {
+		
+		ayylmao = GameObject.FindGameObjectWithTag("AyyLmao").GetComponent<DaControls>();
+	}
+	
+	void Update() {
+		
+		
+		if(Vector3.Distance(ayylmao.GetTransform().position, thisTransform.position) < minDistance) {
+			
+			Vector3 direction = thisTransform.position - ayylmao.GetTransform ().position;
+			direction.Normalize();
+			thisTransform.position = Vector3.MoveTowards(thisTransform.position, direction * minDistance, Time.deltaTime * fest);
+		}
+	}
+}
