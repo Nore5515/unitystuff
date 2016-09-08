c71c7ce06da9d9c22fc85df93f2a490435a04ecb
CtrlPwnDelete <jakezeiner@gmail.com>
2016-09-08 13:36:56 -0500

    Add files via upload

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+
+[AddComponentMenu ("Control Script/FPSInput")]
+
+public class FPSInput : MonoBehaviour 
+{
+	public float speed = 6.0f;
+	private CharacterController _charController;
+	public float gravity = 20.0f;
+	public float jumpSpeed = 8.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Start () 
+	{
+		_charController = GetComponent<CharacterController>();
+	}
+
+	void Update () 
+	{
+		if (_charController.isGrounded)
+		{
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+
+			if (Input.GetButton ("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+			}
+			
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		_charController.Move (moveDirection * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/JumpFly.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpFly : MonoBehaviour {
+	
+	public float jumpStrength;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown ("Jump")) {
+			Debug.Log ("Jumping");
+			this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpStrength, 0.0f));
+			
+		}
+		
+	}
+}
