ce5ba4933da8d068b61bf8272f2d255c884c567e
Kinghawk159 <littlezeeman@gmail.com>
2016-09-16 13:12:45 -0500

    About EnemyDestroy
    Apply to the buildings and make the collider bigger so the enemy can destroy the buuilding

---
+++ Scripts/EnemyDestroy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyDestroy : MonoBehaviour
+{
+	GameObject player;                         
+	public bool playerInRange;                         
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+
+	}
+	
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the entering collider is the player...
+		if (other.gameObject == player) {
+			// ... the player is in range.
+			playerInRange = true;
+			Debug.Log("Destroy");
+		}
+
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		// If the exiting collider is the player...
+		if (other.gameObject == player) {
+			// ... the player is no longer in range.
+			playerInRange = false;
+		}
+	}
+	
+	
+	void Update ()
+	{
+		// Add the time since Update was last called to the timer.
+		if (playerInRange) {
+			Destroy (gameObject);
+			
+			
+			
+		}
+		}
+}
+	
+	
+	
