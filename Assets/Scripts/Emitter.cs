3559e8921ec5b8ed0d9704ac85c8eccd93ca0c65
CtrlPwnDelete <jakezeiner@gmail.com>
2016-09-16 13:50:33 -0500

    Add files via upload

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+	
+	public int particlesPerSec = 100;
+	public ParticleSystem fireBreath;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		fireBreath = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton (0))
+		{
+			fireBreath.Emit((int)(particlesPerSec * Time.deltaTime));
+		}
+	}
+}
