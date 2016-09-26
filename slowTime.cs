76b360b5b8dd08015c48333a28aa89c8e1d63d17
CtrlPwnDelete <jakezeiner@gmail.com>
2016-09-26 14:11:19 -0500

    Add files via upload

--- slowTime.cs
+++ slowTime.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class slowTime : MonoBehaviour {
+
+	public float sloSpeed = 0.1f;
+	public float totalTime = 10f;
+	public float recoveryRate = 0.5f;
+	//public Slider EnergyBar;
+
+	private float elapsed = 0f;
+	private bool isSlow = false;
 
 	// Use this for initialization
 	void Start ()
@@ -12,14 +21,39 @@ public class slowTime : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.Q))
+		if (Input.GetButtonDown ("Q") && elapsed < totalTime)
 		{
-			Time.timeScale = 0.25f;
+			SetSpeed (sloSpeed);
 		}
 
-		if (Input.GetKeyUp (KeyCode.Q))
+		if (Input.GetButtonUp ("Q"))
 		{
-			Time.timeScale = 1;
+			SetSpeed (1f);
 		}
+
+		if (isSlow)
+		{
+			elapsed += Time.deltaTime / sloSpeed;
+
+			if (elapsed >= totalTime)
+			{
+				SetSpeed (1f);
+			}
+		} 
+		else
+		{
+			elapsed -= Time.deltaTime * recoveryRate;
+			elapsed = Mathf.Clamp (elapsed, 0, totalTime);
+		}
+
+		float remainingtime = (totalTime - elapsed) / totalTime;
+		//EnergyBar.value = remainingtime;
+	}
+
+	private void SetSpeed (float speed)
+	{
+		Time.timeScale = speed;
+		Time.fixedDeltaTime = 0.02f * speed;
+		isSlow = !(speed >= 1.0f);
 	}
 }
