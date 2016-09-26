a86c29c5f1e02a9e8b54bb0754970e1e4831e1d6
CtrlPwnDelete <jakezeiner@gmail.com>
2016-09-26 13:53:34 -0500

    Add files via upload

---
+++ slowTime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slowTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Q))
+		{
+			Time.timeScale = 0.25f;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Q))
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
---
+++ speedBoost.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speedBoost : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GameObject Player = GameObject.Find("Player");
+		FPSInput moveScript = Player.GetComponent<FPSInput>();
+		if (Input.GetKeyDown (KeyCode.Mouse1))
+		{
+			moveScript.speed = 60.0f;
+		}
+		
+		if (Input.GetKeyUp (KeyCode.Mouse1))
+		{
+			moveScript.speed = 6.0f;
+		}
+	}
+}
