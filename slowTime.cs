064847305b70d3504b41f9cdcb673e347ee23d9a
CtrlPwnDelete <jakezeiner@gmail.com>
2016-09-30 12:47:24 -0500

    Add files via upload

---
+++ slowTime.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slowTime : MonoBehaviour {
+	
+	float currentAmount = 0f;
+	float maxAmount = 5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown (KeyCode.Z)){
+			
+			if(Time.timeScale == 1.0f)
+				Time.timeScale = 0.3f;
+			
+			else
+				
+				Time.timeScale = 1.0f;
+			Time.fixedDeltaTime = 0.02f * Time.timeScale;
+		}
+		
+		
+		if(Time.timeScale == 0.03f){
+			
+			currentAmount += Time.deltaTime;
+		}
+		
+		if(currentAmount > maxAmount){
+			
+			currentAmount = 0f;
+			Time.timeScale = 1.0f;
+			
+		}
+		
+	}
+}
